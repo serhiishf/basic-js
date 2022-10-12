497302b0c2da6932a1cdc6589d969ca2007aeff9
serhiishf <unison447@gmail.com>
2022-10-12 15:26:00 +0300

    feat: Get-email-dome Done

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,8 +10,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
+function getEmailDomain(email/* email */) {
+  let result = email.replace(/([^\n]+)+@/, '');
+  return result
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date/* date */) {
+  console.log(date);
+  console.log('Console workkk')
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
