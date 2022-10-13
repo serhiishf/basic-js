d6384992254059d7e4957d5c2d32dfd39c803aaa
Serhii Sh <unison447@gmail.com>
2022-10-13 21:45:24 +0300

    feat: Feat Dns-stats Done

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  const result = {};
+  //const regex = /[[:alnum:]]+\./g;
+  domains.forEach((elem) => {
+    const tempArr = [];
+    let str = ".";
+    let domen = "";
+    for (let i = 0; i < elem.length; i++) {
+      if (i === elem.length - 1) {
+        str += elem[i];
+        tempArr.unshift(str);
+        str = ".";
+        break;
+      }
+      if (elem[i] != ".") {
+        str += elem[i];
+      } else if (elem[i] === ".") {
+        tempArr.unshift(str);
+        str = ".";
+      }
+    }
+    tempArr.forEach((itemTempArr) => {
+      domen += itemTempArr;
+      if (result.hasOwnProperty(domen)) {
+        result[`${domen}`]++;
+      } else if (!result.hasOwnProperty(domen)) {
+        result[`${domen}`] = 1;
+      }
+    });
+  });
+  return result;
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
