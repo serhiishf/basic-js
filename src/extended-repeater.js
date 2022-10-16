abb484da798910793c50a6b7992d115bf980178d
Serhii Sh <unison447@gmail.com>
2022-10-16 17:15:46 +0300

    feat: Resolved task Extended-repeater Done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,30 +1,32 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
 function repeater(str, options) {
   //значения по умолчанию - переобпределить из объекта
-  let separator = '+';
-  let additionSeparator = '|';
+  let separator = "+";
+  let additionSeparator = "|";
   let repeatTimes = 1;
-  let addition = '';
+  let addition = "";
   let additionRepeatTimes = 1;
   // итоговые значения
-  let additPart = '';
-  let result = '';
+  let additPart = "";
+  let result = "";
+  //let str = isString(str2);
+  //console.debug(str)
   //все значения проверять на строку, если не строка проверять на номер
   if (!!options.separator) {
     separator = options.separator;
@@ -36,24 +38,54 @@ function repeater(str, options) {
     repeatTimes = options.repeatTimes;
   }
   if (!!options.addition) {
-    addition = options.addition;
+    //addition = options.addition;
+    if(options.addition.toString() === `[object Object]` && options.addition === str){
+       addition = str;
+    } else{
+        addition = isString(options.addition);
+        console.debug(addition);
+    }
+    
+  } else if(!!options.addition === false && typeof(options.addition) === 'boolean') {
+      addition = isString(options.addition)
+  } else if(!!options.addition === false && options.addition === null){
+      addition = 'null'
   }
   if (!!options.additionRepeatTimes) {
     additionRepeatTimes = options.additionRepeatTimes;
   }
   //функция проверки на строку
   function isString(str) {
-    if (typeof str === 'string') {
+    if (typeof str === "string") {
       return str;
+    } else if (str === null) {
+      return "null";
+    }/*  else if(str.toString === `[object Object]` && str.){
+        console.debug('OBJEEEEEECTTT')
+        return str.hint
+    } */
+    else {
+      return str.toString();
     }
+
+    /* else if (typeof str === "number") {
+      return str.toString();
+    } else if (typeof str === "boolean") {
+      return typeof str;
+    } else if (str === null) {
+      return "null";
+    } else if (str === undefined) {
+      return 'STRING_OR_DEFAULT';
+    } else if(str){
+
+    } */
   }
   //функция сборки addition
   function getAddition() {
     for (i = 0; i < additionRepeatTimes; i++) {
       if (i != additionRepeatTimes - 1) {
-        additPart += addition + additionSeparator
-      }
-      else {
+        additPart += addition + additionSeparator;
+      } else {
         additPart += addition;
       }
     }
@@ -66,19 +98,17 @@ function repeater(str, options) {
       } else {
         result += str + additPart;
       }
-
     }
   }
-  getAddition()
-  getResult()
+  getAddition();
+  getResult();
 
-  return result
-
+  return result;
 
   //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,11 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,12 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
+  calculateDepth(arr) {
     throw new NotImplementedError('Not implemented');
+    
+    if (Array.isArray(array)) {
+      return 1 + Math.max(0, ...array.map(calculateDepth));
+    } else {
+      return 0;
+    }
+    /*         function recurs(array) { 
+          if (Array.isArray(array)) { 
+            return 1 + Math.max(0, ...array.map(recurs)) 
+          } else { 
+            return 0 
+          } 
+        
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+  } */
+    return recurs(arr);
   }
 }
 
 module.exports = {
-  DepthCalculator
+  DepthCalculator,
 };
